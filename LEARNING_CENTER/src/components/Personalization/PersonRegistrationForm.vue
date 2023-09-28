98b8bc2a719da4a26f306b9132acd8bdda9b62cb
Eugeny Butov <esbutov@stud.etu.ru>
2023-09-28 20:33:28 +0300

    Added text fields to PersonRegistrationForm

--- LEARNING_CENTER/src/components/Personalization/PersonRegistrationForm.vue
+++ LEARNING_CENTER/src/components/Personalization/PersonRegistrationForm.vue
@@ -1,4 +1,7 @@
 <script setup>
+import BaseInputSlot from '../GeneralComponents/BaseInputSlot.vue';
+import { ref } from 'vue';
+
 const props = defineProps({
     open: {
         type: Boolean,
@@ -6,20 +9,46 @@ const props = defineProps({
     },
     person: {
         type: Object,
-        default(rawProps) {
+        default() {
             return {
-                name: 'Имя',
-                age: 13,
-                height: 170,
+                name: null,
+                age: 0,
+                height: 0,
                 weight: 0,
-                gender: 'female',
-                activity: 'none',
+                gender: null,
+                activity: null,
             }
         }
     }
 });
 const emit = defineEmits(['update:open']);
 
+const formFieldsInfo = ref([
+    {
+        title: 'Имя',
+        value: '',
+        pattern: /^[А-Я][а-я]{1,}$/,
+        type: 'текст'
+    },
+    {
+        title: 'Рост (см)',
+        value: '',
+        pattern: /^1[3-9][0-9]$|^2[0-5][0-9]$/, //Рост между 130 и 259 см (251 - самый высокий)
+        type: 'число в диапазоне 130-259'
+    },
+    {
+        title: 'Вес (кг)',
+        value: '',
+        pattern: /^[3-9][0-9]$|^[1-4][0-9]{1,2}$/,
+        type: 'число в диапазоне 30-499' //Рекордный вес на данный момент составил 485 кг
+    },
+    {
+        title: 'Возраст',
+        value: '',
+        pattern: /^1[3-9]$|^[2-9][0-9]$/,
+        type: 'число в диапазоне 13-99' // Людям за 99+ уже не особо надо...
+    }
+])
 
 function close() {
     console.log('close');
@@ -34,6 +63,12 @@ function close() {
 persistent>
     <v-card>
         <v-card-title>Регистрация</v-card-title>
+        <base-input-slot v-for="field in formFieldsInfo"
+            :value="field.value"
+            :name="field.title"
+            :pattern="field.type"
+            :test="field.pattern"
+            @change="field.value = $event.value"/>
         <v-card-action>
             <v-btn @click="close">Закрыть</v-btn>
         </v-card-action>
