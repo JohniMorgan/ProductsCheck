ecd8ecb8512360217eab34fe16fc1fcab650ba46
Eugeny Butov <esbutov@stud.etu.ru>
2023-10-04 20:12:19 +0300

    added Registration person to localStorage

--- LEARNING_CENTER/src/components/Personalization/PersonRegistrationForm.vue
+++ LEARNING_CENTER/src/components/Personalization/PersonRegistrationForm.vue
@@ -1,6 +1,7 @@
 <script setup>
 import BaseInputSlot from '../GeneralComponents/BaseInputSlot.vue';
 import { ref } from 'vue';
+import { usePersonStore } from '../../store/person';
 
 const props = defineProps({
     open: {
@@ -49,11 +50,66 @@ const formFieldsInfo = ref([
         type: 'число в диапазоне 13-99' // Людям за 99+ уже не особо надо...
     }
 ])
+const formRadio = ref('male');
 
 function close() {
     console.log('close');
     emit('update:open', false);
 };
+
+const formSelectInfo = ref([
+    {
+        value: 'min',
+        title: 'Минимальная активность',
+        subtitle: 'Сидячая работа, без физической активности',
+    },
+    {
+        value: 'low',
+        title: 'Слабая активность',
+        subtitle: 'Небольшие пробежки, лёгкая гимнастика 1-3 раза в неделю или частые пешие прогулки',
+    },
+    {
+        value: 'mid',
+        title: 'Умеренная активность',
+        subtitle: 'Интенсивные тренировки 3-5 раз в неделю',
+    },
+    {
+        value: 'hard',
+        title: 'Высокая активность',
+        subtitle: 'Интенсивные занятия спортом 5-7 раз в неделю или тяжёлая работа',
+    },
+    {
+        value: 'extra',
+        title: 'Экстремальная активность',
+        subtitle: 'Очень трудоёмкая работа, ежедневные тренировки в несколько подходов',
+    }
+])
+
+const selectedItem = ref('min');
+
+const form = ref(null);
+
+async function validate() {
+    const {valid} = await form.value.validate();
+
+    if (valid) registrationSubmit();
+};
+
+function registrationSubmit() {
+    const fullFormData = {
+        name: formFieldsInfo.value[0].value,
+        age: formFieldsInfo.value[3].value,
+        height: formFieldsInfo.value[1].value,
+        weight: formFieldsInfo.value[2].value,
+        gender: formRadio.value,
+        activity: selectedItem.value,
+    }
+    const store = usePersonStore();
+
+    store.registerPerson(fullFormData);
+    close();
+};
+
 
 </script>
 
@@ -63,20 +119,50 @@ function close() {
 persistent>
     <v-card>
         <v-card-title>Регистрация</v-card-title>
+        <v-form ref="form">
         <base-input-slot v-for="field in formFieldsInfo"
             :value="field.value"
             :name="field.title"
             :pattern="field.type"
             :test="field.pattern"
             @change="field.value = $event.value"/>
-        <v-card-action>
-            <v-btn @click="close">Закрыть</v-btn>
-        </v-card-action>
+        <v-radio-group
+            label="Пол"
+            v-model="formRadio"
+            inline>
+            <v-radio value="male" label="Мужской"></v-radio>
+            <v-radio value="female" label="Женский"></v-radio>
+        </v-radio-group>
+        <v-select :items="formSelectInfo"
+        v-model="selectedItem">
+            <template v-slot:item="{props, item}">
+                <v-list-item v-bind="props" :title="item.raw.title">
+                    <v-list-item-subtitle class="text-wrap">{{ item.raw.subtitle }}</v-list-item-subtitle>
+                </v-list-item>
+            </template>
+        </v-select>
+        </v-form>
+    <v-card-action>
+        <v-btn @click="close">Закрыть</v-btn>
+        <v-btn @click="validate">Chek-In</v-btn>
+    </v-card-action>
     </v-card>
 </v-dialog>
 
 </template>
 
 <style scoped lang='scss'>
+    .v-card {
+        max-width: 400px;
+        width: 40%;
+        min-width: 300px;
+    }
+    .v-list-item {
+        max-width: 300px;
+        min-width:300px;
+    }
 
+    .text-wrap {
+        -webkit-line-clamp: unset !important;
+    }
 </style>
--- LEARNING_CENTER/src/store/person.js
+++ LEARNING_CENTER/src/store/person.js
@@ -18,11 +18,45 @@ export const usePersonStore = defineStore('person', {
         init() {
             const initData = localStorage.getItem(ITEM_TAG);
             if (initData) {
+                console.log('Обнаружены данные');
                 this.person = JSON.parse(initData);
             } else {console.log('Не найдено данных пользователя')}
         },
         updateStorage() {
             localStorage.setItem(ITEM_TAG, JSON.stringify(this.person));
+        },
+        calculateDayCalories() {
+            const genderRatio = this.person.gender == 'male' ? 5 : - 161;
+            let activityRatio = null;
+            switch (this.person.activity) {
+                case 'min':
+                    activityRatio = 1.2;
+                    break;
+                case 'low':
+                    activityRatio = 1.375;
+                    break;
+                case 'mid':
+                    activityRatio = 1.55;
+                    break;
+                case 'hard':
+                    activityRatio = 1.7;
+                    break;
+                case 'extra':
+                    activityRatio = 1.9;
+                    break;
+            }
+
+            let count = 10*this.person.weight + 6.25*this.person.height;
+            count += -5*this.person.age + genderRatio;
+            count *= activityRatio;
+            count = Math.ceil(count);
+            this.person.dayCount = count;
+        },
+        registerPerson(formData) {
+            this.person = formData;
+            console.log(this.person);
+            this.calculateDayCalories();
+            this.updateStorage();
         }
     }
 });
